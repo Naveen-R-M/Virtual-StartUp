d6d8e8070fd4b8ed4032e77e3c07c200ac9ef523
Naveen-R-M <naveen.rm.551@gmail.com>
2021-03-19 10:10:24 +0530

    AES sqlite format

--- Backend/aes.py
+++ Backend/aes.py
@@ -9,7 +9,7 @@ class Aes:
     def encrypt(self, message):
         cipher = AES.new(self.key, AES.MODE_EAX)
         nonce = cipher.nonce
-        ciphertext, tag = cipher.encrypt_and_digest(message.encode('ascii'))
+        ciphertext, tag = cipher.encrypt_and_digest(message.encode('ascii'))   
         return nonce, ciphertext, tag
 
     def decrypt(self, nonce, ciphertext, tag):
@@ -25,8 +25,12 @@ aes = Aes()
 nonce, ciphertext, tag = aes.encrypt("Hello Naveen..")
 plaintext = aes.decrypt(nonce=nonce, ciphertext=ciphertext, tag=tag)
 print('cipher',ciphertext)
+ciphertext = str(ciphertext)
+ciphertext = ciphertext.replace("'","")
+ciphertext = ciphertext.strip('b')
+print(type(ciphertext))
 print(f'cipher text {ciphertext}')
-print(f'b64 : {str(base64.b64encode(ciphertext))}')
+
 if not plaintext:
     print('Corrupted')
 else:
